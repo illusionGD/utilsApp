d20972bb5e559a8a9d25dbce6cf113efcf8f0ea1
tanjinde <jinde.tan@play800.cn>
2024-12-31 15:31:51 +0800

    删除文件

--- src/utils/constant.ts
+++
@@ -1,5 +0,0 @@
-export const LOCAL_STORAGE_KEY = {
-    pressImgConfig: 'config_press_img',
-    modifyFileNameConfig: 'config_modify_file_name',
-    bitmapFntConfig: 'config_bitmap_fnt',
-}
--- src/views/home.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="home flex-center p-ab-center">
-        <img src="../../public/logo.png" alt="" />
-    </div>
-</template>
